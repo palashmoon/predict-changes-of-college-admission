4299bb585ad6918b28d2eade620b71b22caf6a5a
palashmoon <palashmoon8@gmail.com>
2020-03-26 23:14:37 +0530

    file added

---
+++ main1.py
@@ -0,0 +1,42 @@
+from flask import Flask , render_template , request,jsonify
+from flask_cors import CORS,cross_origin
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+@cross_origin()
+def homePage():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST' , 'GET'])
+@cross_origin()
+def index():
+    if request.method == 'POST':
+        try:
+            gre_score = float(request.form['gre_score'])
+            tofle_score = float(request.form['tofle_score'])
+            university_rating = float(request.form['university_rating'])
+            sop = float(request.form['sop'])
+            lor = float(request.form['lor'])
+            cgpa = float(request.form['cgpa'])
+            is_research = request.form['research']
+            if(is_research == 'yes'):
+                research = 1
+            else:
+                research = 0
+            filename = 'finalized_model.pickle'
+            loaded_model = pickle.load(open(filename, 'rb'))  # loading the model file from the storage
+            # predictions using the loaded model file
+            prediction = loaded_model.predict([[gre_score, toefl_score, university_rating, sop, lor, cgpa, research]])
+            print('prediction is', prediction)
+            # showing the prediction results in a UI
+            return render_template('results.html', prediction=round(100 * prediction[0]))
+        except Exception as e:
+            print('The Exception message is: ',e)
+            return 'something is wrong'
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
